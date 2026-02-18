25b1d26a8232e503284bc5b5ad3be76dfb6ce28f
laibasher77-tech <laiba.sher77@gmail.com>
2026-02-18 15:42:58 +0500

    neeew expermient

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import NavbarComponent from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Textforms from './components/textform'
-// import { useState } from 'react';
-// import Alert from './components/alert';
+import Textforms from './components/textform'
+import { useState } from 'react';
+import Alert from './components/alert';
 // import About from './components/about';
 // import { Routes, Route } from 'react-router';
 // import Newthing from './components/newthing'
@@ -12,73 +12,73 @@ import Breakprac from './components/breakprac';
 
  
 function App() {
-  // const [mode,setMode]= useState('light')
-  // const [alert,setAlert]=useState(null)
-  // const alerting=(message,type)=>{
-  //     setAlert({
-  //       msg:message,
-  //       type:type
-  //     })
-  //    setTimeout(() => {
-  //      setAlert(null)
-  //    }, 2000);
-  //      }
-  // const toggleMode=()=>{
-  //   if(mode==='light'){
-  //     setMode('dark')
-  //     document.body.style.backgroundColor="black"
-  //     alerting("Dark Mode is enabled","success")
+  const [mode,setMode]= useState('light')
+  const [alert,setAlert]=useState(null)
+  const alerting=(message,type)=>{
+      setAlert({
+        msg:message,
+        type:type
+      })
+     setTimeout(() => {
+       setAlert(null)
+     }, 2000);
+       }
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="black"
+      alerting("Dark Mode is enabled","success")
       
-  //   }else{
-  //     setMode('light')
-  //      document.body.style.backgroundColor="white"
-  //     alerting("light Mode is enabled","success")
+    }else{
+      setMode('light')
+       document.body.style.backgroundColor="white"
+      alerting("light Mode is enabled","success")
      
-  //   }
-  //   }
-  //   const greentoggleMode=()=>{
-  //   if(mode==='light'){
-  //     setMode('dark')
-  //     document.body.style.backgroundColor="grey"
-  //     alerting("Dark Mode is enabled","success")
+    }
+    }
+    const greentoggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="grey"
+      alerting("Dark Mode is enabled","success")
       
-  //   }else{
-  //     setMode('light')
-  //      document.body.style.backgroundColor="white"
-  //     alerting("light Mode is enabled","success")
+    }else{
+      setMode('light')
+       document.body.style.backgroundColor="white"
+      alerting("light Mode is enabled","success")
      
-  //   }
-  //   }
-  //   const redtoggleMode=()=>{
-  //   if(mode==='light'){
-  //     setMode('dark')
-  //     document.body.style.backgroundColor="#dc3545"
-  //     alerting("Dark Mode is enabled","success")
+    }
+    }
+    const redtoggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="#dc3545"
+      alerting("Dark Mode is enabled","success")
       
-  //   }else{
-  //     setMode('light')
-  //      document.body.style.backgroundColor="white"
-  //     alerting("light Mode is enabled","success")
+    }else{
+      setMode('light')
+       document.body.style.backgroundColor="white"
+      alerting("light Mode is enabled","success")
      
-  //   }
-  //   }
-  //   const bluetoggleMode=()=>{
-  //   if(mode==='light'){
-  //     setMode('dark')
-  //     document.body.style.backgroundColor="blue"
-  //     alerting("Dark Mode is enabled","success")
+    }
+    }
+    const bluetoggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor="blue"
+      alerting("Dark Mode is enabled","success")
       
-  //   }else{
-  //     setMode('light')
-  //      document.body.style.backgroundColor="white"
-  //     alerting("light Mode is enabled","success")
+    }else{
+      setMode('light')
+       document.body.style.backgroundColor="white"
+      alerting("light Mode is enabled","success")
      
-  //   }
-  //   }
+    }
+    }
     
   return (
     <>
-      {/* <NavbarComponent 
+      <NavbarComponent 
         title="Laiba" 
         mode={mode} 
         toggleMode={toggleMode} 
@@ -88,7 +88,7 @@ function App() {
       />
 
       <Alert alert={alert} />
-      <Textforms mode={mode} alerting={alerting} />
+ <Textforms mode={mode} alerting={alerting} />
 
        {/* <Routes>
         <Route path="/" element={<Textforms mode={mode} alerting={alerting} />}  />
